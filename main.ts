ede33c19b33299ba001956caae8a64ae3b14f0ce
Nim2010 <159002190+Nim2010@users.noreply.github.com>
2024-02-05 09:19:39 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(2000)
+    basic.clearScreen()
+    basic.pause(2000)
+})
